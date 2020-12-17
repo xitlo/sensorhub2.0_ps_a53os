c9718562dfedaf3b6ee8b4bdc19a0e9f171e7606
hanfangzheng <hanfangzheng@momenta.ai>
2020-12-17 19:10:10 +0800

    Description: modify autostart for partition remount; add deploy script and config file
    Module: rootfs
    
    BugID: null
    
    Product: sensorhub2-ps-a53
    
    Signed-off-by: hanfangzheng <hanfangzheng@momenta.ai>

---
+++ project-spec/meta-user/recipes-apps/common/common.bb
@@ -0,0 +1,20 @@
+#
+# This file is the common recipe.
+#
+
+SUMMARY = "Simple common application"
+SECTION = "PETALINUX/apps"
+LICENSE = "MIT"
+LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/MIT;md5=0835ade698e0bcf8506ecda2f7b4f302"
+
+SRC_URI = "file://deploy.sh \
+	file://sensorhub2-config.json \
+	"
+
+S = "${WORKDIR}"
+
+do_install() {
+	     install -d ${D}${sysconfdir}/common
+	     install -m 0755 ${S}/deploy.sh ${D}${sysconfdir}/common
+		 install -m 0644 ${S}/sensorhub2-config.json ${D}${sysconfdir}/common
+}
---
+++ project-spec/meta-user/recipes-apps/common/files/deploy.sh
@@ -0,0 +1,83 @@
+#!/bin/sh
+set -e
+
+run_mmc_case()
+{
+    # create 3 partitions
+    fdisk /dev/mmcblk0 2>&1 1>/dev/null  << EOF
+    p
+    d
+    1
+    d
+    2
+    d
+    3
+    d
+    n
+    p
+    1
+
+    +512M
+    t
+    b
+    n
+    p
+    2
+
+    +512M
+    n
+    p
+    3
+
+
+    w
+EOF
+    sleep 2
+    if [ "$?" = 0 ]; then
+        if [ -e '/dev/mmcblk0p1' ] && [ -e '/dev/mmcblk0p2' ] && [ -e '/dev/mmcblk0p3' ]; then
+            echo -e "MMC part ok! \n\n"
+            return
+        fi
+    fi
+    echo -e "MMC test fails \n\n"
+}
+
+#1, umount
+echo -e ">>1, df & umount &fdisk\n"
+df
+umount /dev/mmcblk0p*
+fdisk -l
+
+#2, re-partition
+echo -e ">>2, fdisk\n"
+run_mmc_case
+
+#3, format partitions
+sleep 1
+echo -e ">>3, format\n"
+umount /dev/mmcblk0p*
+mkfs.vfat /dev/mmcblk0p1
+mkfs.ext4 -E nodiscard -F /dev/mmcblk0p2
+mkfs.ext4 -E nodiscard -F /dev/mmcblk0p3
+
+#4, remount
+echo -e ">>4, remount\n"
+if [ ! -d /boot ]; then
+    mkdir /boot
+fi
+
+if [ ! -d /ldc ]; then
+    mkdir /ldc
+fi
+
+if [ ! -d /data ]; then
+    mkdir /data
+fi
+
+mount -o rw /dev/mmcblk0p1 /boot
+mount -o rw /dev/mmcblk0p2 /ldc
+mount -o rw /dev/mmcblk0p3 /data
+
+#5, done
+echo -e ">>5, all done\n"
+
