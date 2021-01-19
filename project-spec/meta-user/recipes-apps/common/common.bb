f87bab528211a73267c6434b64483e7a9fb8aa41
hanfangzheng <hanfangzheng@momenta.ai>
2021-01-19 13:31:03 +0800

    Description: add test_time.sh
    Module: rootfs
    
    BugID: null
    
    Product: sensorhub2-ps-a53
    
    Signed-off-by: hanfangzheng <hanfangzheng@momenta.ai>

--- project-spec/meta-user/recipes-apps/common/common.bb
+++ project-spec/meta-user/recipes-apps/common/common.bb
@@ -10,6 +10,7 @@ LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/MIT;md5=0835ade698e0bcf8506ecda
 SRC_URI = "file://deploy.sh \
 	file://sensorhub2-config.json \
 	file://zlog.conf \
+	file://test_time.sh \
 	"
 
 S = "${WORKDIR}"
@@ -19,4 +20,5 @@ do_install() {
 		install -m 0755 ${S}/deploy.sh ${D}${sysconfdir}/common
 		install -m 0644 ${S}/sensorhub2-config.json ${D}${sysconfdir}/common
 		install -m 0644 ${S}/zlog.conf ${D}${sysconfdir}/common
+		install -m 0755 ${S}/test_time.sh ${D}${sysconfdir}/common
 }
---
+++ project-spec/meta-user/recipes-apps/common/files/test_time.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+#1, check param
+if [ $# -lt 1 ] ; then
+   echo "Usage: $0 [timesync_period_ms] <if_only_run_test> <debug_swtich>"
+   echo " e.g.: $0 2000"
+   exit 1;
+fi
+
+#2, check if only swtich debug
+if [ $# -gt 2 ] ; then
+   echo "only set timesync_debug_log $3"
+   echo $3 > /sys/devices/platform/timesync/timesync_debug_log
+   cat /sys/devices/platform/timesync/timesync_debug_log
+   exit 0;
+fi
+
+#3, check if only run timesync-test
+if [ $# -gt 1 -a $2 -eq 1 ] ; then
+   echo "only run timesync-test with para $1"
+   /data/APP/timesync-test /dev/timesync $1
+   exit 0;
+fi
+
+#4, enable timer and set time to 20210101-000000
+mem-test w 0x80000000 0x24 0x00
+#mem-test w 0x80000000 0x04 0x20000000
+mem-test w 0x80000000 0x04 0x5FEDF580
+mem-test w 0x80000000 0x24 0x01
+
+#5, load timesync driver
+lsmod | grep timesync
+if [ $? -eq 0 ]; then
+    echo "remove timesync driver"
+    rmmod timesync.ko
+    sleep 1
+fi
+
+echo "load timesync driver"
+insmod /lib/modules/5.4.0-xilinx-v2020.1/extra/timesync.ko
+sleep 2
+
+#6, run timesync-test
+#/data/APP/timesync-test /dev/timesync $1
+timesync-app /dev/timesync $1
