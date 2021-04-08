b366d7afc10cdf023fc9cac56c819d8a6a1e980d
hanfangzheng <hanfangzheng@momenta.ai>
2021-04-07 21:02:10 -0600

    Description: update deploy emmc script
    Module: deploy
    
    BugID: null
    
    Product: sensorhub2-ps-a53
    
    Signed-off-by: hanfangzheng <hanfangzheng@momenta.ai>

--- project-spec/meta-user/recipes-apps/momenta/files/tools/deploy-emmc.sh
+++ project-spec/meta-user/recipes-apps/momenta/files/tools/deploy-emmc.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-SCRIPT_VERSION=v2.1
+SCRIPT_VERSION=v2.3
 set -e
 
 run_mmc_case()
@@ -45,11 +45,31 @@ EOF
 
 echo -e "$0 VER: $SCRIPT_VERSION"
 
+#0, check param
+echo -e "\n>>0, check param"
+if [ $# -lt 1 ] ; then
+	echo -e "Usage: $0 <dir_of_firmware>"
+	echo -e "\tdir_of_firmware, could be NULL if only partition"
+	echo -e "\te.g. $0 /media/sd-mmcblk1p1"
+    exit 1
+fi
+
+if [ "NULL" != "$1" ]; then
+    PATH_BOOT_BIN=$1/BOOT.BIN
+    PATH_BOOT_SCR=$1/boot.scr
+    PATH_IMG_UB=$1/image.ub
+
+    if [ ! -f $PATH_BOOT_BIN ] || [ ! -f $PATH_BOOT_SCR ] || [ ! -f $PATH_IMG_UB ]; then
+        echo -e "can't find BOOT.BIN/boot.scr/image.ub, pls check!"
+        exit 1
+    fi
+fi
+
 #1, umount
 echo -e "\n>>1, df & umount &fdisk"
 df
 has_mount=`df | grep mmcblk0 | head -1  | awk '{print $1}'`
-while [ "" != $has_mount ]
+while [ "" != "$has_mount" ]
 do
     umount $has_mount
     has_mount=`df | grep mmcblk0 | head -1  | awk '{print $1}'`
@@ -65,7 +85,7 @@ run_mmc_case
 sleep 1
 echo -e "\n>>3, format"
 has_mount=`df | grep mmcblk0 | head -1  | awk '{print $1}'`
-while [ "" != $has_mount ]
+while [ "" != "$has_mount" ]
 do
     umount $has_mount
     has_mount=`df | grep mmcblk0 | head -1  | awk '{print $1}'`
@@ -93,5 +113,18 @@ mount -o rw /dev/mmcblk0p1 /boot
 mount -o rw /dev/mmcblk0p2 /ldc
 mount -o rw /dev/mmcblk0p3 /data
 
-#5, done
-echo -e "\n>>5, all done"
+#5, copy firmware to /boot
+echo -e "\n>>5, check for firmware copy"
+if [ "NULL" != $1 ]; then
+    echo -e ">>5.1, copy firmware to /boot"
+    cp -rf $PATH_BOOT_BIN /boot && sync
+    cp -rf $PATH_BOOT_SCR /boot && sync
+    cp -rf $PATH_IMG_UB /boot && sync
+    cp -rf /boot/BOOT.BIN /boot/BOOT0002.BIN && sync
+    cp -rf /boot/image.ub /boot/image0002.ub && sync
+else
+    echo -e ">>5.2, no path for firmware, skip"
+fi
+
+#6, done
+echo -e "\n>>6, all done"
